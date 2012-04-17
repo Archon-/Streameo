46a7a3419de74ebf8d133e28acbd049042db5d38
t <t>
2012-04-17 09:33:18 +0200

    player controller

---
+++ Streameo/Controllers/PlayerController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace Streameo.Controllers
+{
+    public class PlayerController : Controller
+    {
+        //
+        // GET: /Player/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Listen(int id)
+        {
+            string file = "";
+            if (id == 1)
+                file = Server.MapPath("~/Music/Tesserakt/The Uknown/03 111.mp3");
+            else if (id == 2)
+                file = Server.MapPath("~/Music/Tesserakt/The Uknown/01 Ultrarozpiedalator.mp3");
+
+            using (var file1 = new FileStream(file, FileMode.Open))
+            {
+                using (var stream = new MemoryStream())
+                {
+                    byte[] buffer = new byte[32768];
+                    int read;
+                    while ((read = file1.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        stream.Write(buffer, 0, read);
+
+                    }
+                    //return buffer;
+                    //return null;
+                    //stream.Position = 0;
+                    //return stream.
+                }
+            }
+
+
+            return File(file, "audio/mp3");
+
+        }
+
+        //
+        // GET: /Player/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Player/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Player/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /Player/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Player/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Player/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Player/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
