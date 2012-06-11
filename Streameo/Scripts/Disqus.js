aaed252e3fd49a26293586a2aee7b807c15b068e
t <t>
2012-06-11 22:33:12 +0200

    dodane pliki widoku dla wyszukiwania, zapomniałem wcześniej dać

---
+++ Streameo/Scripts/Disqus.js
@@ -0,0 +1,12 @@
+﻿
+
+function successToArtist(result) {
+//    (function () {
+//        var disqus_shortname = 'streameo';
+//        var disqus_developer = 1;
+//        var disqus_identifier = 'StreameoArtistCommentsRise Against';
+//        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+//        dsq.src = 'http://streameo.disqus.com/embed.js';
+//        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+//    })();
+}
